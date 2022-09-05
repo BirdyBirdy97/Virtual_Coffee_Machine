066c88e949ad80de0fc4fc35129a985ff5218969
Rose (Raven) Bishop <97709284+BirdyBirdy97@users.noreply.github.com>
2022-09-05 01:44:18 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,26 @@
+menu = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18
+        },
+        "price": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "price": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 200,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "price": 3,
+    },
+}
